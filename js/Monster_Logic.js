e48c26d110428d46c9e9f2ba7235c5c94bf075f8
Edward Bradley <eabradley@connect.ust.hk>
2018-04-02 16:16:42 +0800

    Fixed an issue involving keypress and keydown. Now, keydown is used to process the arrow keys, while keypress process the spacebar

--- js/Monster_Logic.js
+++ js/Monster_Logic.js
@@ -13,22 +13,26 @@ function setupMonster(pos_num) {
     newMonster.css("transform", "translate(" + Positions[pos_num].position.x.toString() + "px, 525px)");
     newMonster.hide();
 
-    Active_Monsters[pos_num].push(newMonster);
+    Active_Monsters[pos_num].push({obj_ref: newMonster, timeout_func: null});
 
     $("#game-screen").append(newMonster);
 
-    setTimeout(startMonsterMove(newMonster, pos_num), 1000);
+    setTimeout(startMonsterMove({obj_ref: newMonster, timeout_func: null}, pos_num), 500);
 }
 
-function startMonsterMove(monster_obj, monster_pos) {
-    monster_obj.show();
-    monster_obj.css("transform", "translate(" + Positions[monster_pos].position.x.toString() + "px, 100px)");
+function startMonsterMove(monster_entity, monster_pos) {
+    monster_entity.obj_ref.show();
+    monster_entity.obj_ref.css("transform", "translate(" + Positions[monster_pos].position.x.toString() + "px, 100px)");
 
-    setTimeout(function () {
-        endMonsterMove(monster_obj, monster_pos);
+    monster_entity.timeout_func = setTimeout(function () {
+        monsterReachesTop(monster_pos);
     }, 2000);
 }
 
-function endMonsterMove(monster_obj, monster_pos) {
+function monsterReachesTop(monster_pos) {
+    (Active_Monsters[monster_pos].shift()).obj_ref.remove();
+}
+
+function monsterGetsKilled(monster_pos) {
 
 }
